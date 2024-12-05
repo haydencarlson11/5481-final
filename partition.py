bf5444091770940ca7035551c6e5750ba23e5ade
Hayden Carlson <haydenccarlson@gmail.com>
2024-12-05 15:49:56 -0600

    formatted dates

--- partition.py
+++ partition.py
@@ -7,9 +7,12 @@ import sys
 
 def partition_fasta(input_file):
     preCOVID = open("pre-COVID.fasta", "w")
+    preCovidDates = []
     duringCOVID = open("during-COVID.fasta", "w")
+    duringCovidDates = []
     postCOVID = open("post-COVID.fasta", "w")
     fout = None
+    postCovidDates = []
     covidStart = date(2020, 3, 1)
     covidEnd = date(2022, 6, 1)
 
@@ -21,17 +24,32 @@ def partition_fasta(input_file):
                 dateObj = datetime.strptime(dateStr, "%Y/%m/%d").date()
                 if dateObj < covidStart:
                     fout = preCOVID
+                    preCovidDates.append(dateStr.replace("/", "-"))
                 elif dateObj > covidEnd:
                     fout = postCOVID
+                    postCovidDates.append(dateStr.replace("/", "-"))
                 else:
                     fout = duringCOVID
-                fout.write(">"+dateStr+"\n") #we only care about date
+                    duringCovidDates.append(dateStr.replace("/", "-"))
+                
+                fout.write(">"+dateStr.replace("/", "-")+"\n") #we only care about date
             else:
                 fout.write(line + "\n")
 
     preCOVID.close()
     duringCOVID.close()
     postCOVID.close()
+
+    #now write to dates file
+    dates = open('dates.txt', 'w')
+    for dateStr in preCovidDates:
+        dates.write(dateStr + "\n")
+    for dateStr in duringCovidDates:
+        dates.write(dateStr + "\n")
+    for dateStr in postCovidDates:
+        dates.write(dateStr + "\n")
+    dates.close()
     return
+
 
 partition_fasta(sys.argv[1])
